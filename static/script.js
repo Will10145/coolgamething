46ace19ec18ac7a7261e215d10a5f1ad56ddd4fa
Goldcrescent <tom.harker3@hotmail.com>
2025-04-24 20:22:48 +0100

    error incode

--- static/script.js
+++ static/script.js
@@ -3,6 +3,7 @@ const url2 = "https://AeAjHAHiVpRkwZeZZbsZn9Fv-opensocial.googleusercontent.com/
 const url3 = "https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/RobiFet/workflow@60b41ef8c9123ae41b86bf04c9b39e1a5713aaa9/bin/gs.xml&container=ig";
 const url4 = "https://geometry.monster";
 const url5 = "https://serve.gamejolt.net/190223b04e193c0ce0f4c7b9f1bbfd7332533bdc0fcb22d08618d23dbadfa01c,1745605068,7/data/games/15/177/956177/files/6787d14f5ab77/index.html?gjapi_username=Goldcrescent&gjapi_token=WAyjCh";
+const url6 = "https://www.xbox.com/en-us/play";
 
 let win1, win2, win3, win4, win5, win6;
 
@@ -40,5 +41,5 @@ document.getElementById('button2').onclick = () => openGame(url2, 'win2');
 document.getElementById('button3').onclick = () => openGame(url3, 'win3');
 document.getElementById('button4').onclick = () => openGame(url4, 'win4');
 document.getElementById('button5').onclick = () => openGame(url5, 'win5');
-document.getElementById('button6').onclick = () => openGame(url16, 'win6');
+document.getElementById('button6').onclick = () => openGame(ur16, 'win6');
 
